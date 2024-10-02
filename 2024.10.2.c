6e3ff6f30cd0648f84545154bb5ad3da8cf929f4
vrea <vrea42658@gmail.com>
2024-10-02 17:35:45 +0800

    Add files via upload

---
+++ 2024.10.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+	char x='B';
+
+switch(x)
+
+{case'A':printf("ItisA.");
+
+case'B':printf("ItisB.");
+
+case'C':printf("ItisC.");
+
+default:printf("other.");}
+	return 0;
+}
